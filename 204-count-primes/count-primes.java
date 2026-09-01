902d1bd65d7b32de2dfac2fb80a2aa24acd5d6cf
durgaprasad2208 <kdurgaprasad2208@gmail.com>
2026-09-01 13:24:02 +0530

    Time: 703 ms (16.75%) | Memory: 79.2 MB (24.07%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] bool = new boolean[n];
+        for(int i = 0;i<bool.length;i++){
+            bool[i]=true;
+        }
+        for(int i = 2;i<Math.sqrt(n);i++){
+            if(bool[i]==true){
+                for(int j = i*i;j<n;j=j+i){
+                    bool[j]=false;
+                }
+            }
+        }
+        int count = 0;
+        for(int i=2;i<bool.length;i++){
+            if(bool[i]==true){
+                count++;
+            }
+        }
+        return count;
+    }
+}
